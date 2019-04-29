16f86578e77a63468c9272723b9e2a05574a051d
cwiethor <48544014+cwiethor@users.noreply.github.com>
2019-04-28 21:05:25 -0500

    Create jsonphp.php

---
+++ jsonrepo/jsonphp.php
@@ -0,0 +1,8 @@
+{
+    "Json Niagara Falls":{"name":"Json Niagara Falls", "placeid":14303, "category":
+    ["Hiking Json Mountain Trails","Relax Json Great place to obtain peace"]},
+    "Quetico Provincial Park":{"name":"Quetico Provincial Park", "placeid":10007, "category":
+    ["Fishing Great Place to go fishing","Relax Great place to obtain peace"]},
+    "Chicago Willis Tower":{"name":"Chicago Willis Tower", "placeid":60606, "category":
+    ["Friendly Great place to meet new people","Dense This place has a dense population"]},
+}
